679be1ce5070282d7c84a98a93ec157bff599dd4
Naitik Shah <naitik.shah@yahoo.com>
2008-08-30 21:35:46 -0700

    use xhtml for yui rte

--- media/js/rte.js
+++ media/js/rte.js
@@ -1,7 +1,7 @@
 YAHOO.util.Event.onDOMReady(function() {
     YAHOO.util.Dom.addClass(document.body, 'yui-skin-sam');
     var makeRich = function(id, height) {
-        (new YAHOO.widget.Editor(id, {height: height, width: '700px', handleSubmit: true})).render();
+        (new YAHOO.widget.Editor(id, {height: height, width: '700px', markup: 'xhtml', handleSubmit: true})).render();
         window.setTimeout(function() { YAHOO.util.Dom.setStyle(id + '_container', 'margin-left', '108px'); }, 200);
     };
     makeRich('id_excerpt', '200px');
